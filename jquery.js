58daa9bf44446fb1ebcbea554b5bd269016eb028
Kboneil <kelly.brianne.oneil@gmail.com>
2016-09-15 12:54:08 -0500

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -3,22 +3,3 @@ $(document).ready(function() {
     $('expand').slideToggle();
   });
 });
-
-   $(function($){
-        var addToAll = false;
-        var gallery = true;
-        var titlePosition = 'inside';
-        $(addToAll ? 'img' : 'img.fancybox').each(function(){
-            var $this = $(this);
-            var title = $this.attr('title');
-            var src = $this.attr('data-big') || $this.attr('src');
-            var a = $('<a href="#" class="fancybox"></a>').attr('href', src).attr('title', title);
-            $this.wrap(a);
-        });
-        if (gallery)
-            $('a.fancybox').attr('rel', 'fancyboxgallery');
-        $('a.fancybox').fancybox({
-            titlePosition: titlePosition
-        });
-    });
-    $.noConflict();
